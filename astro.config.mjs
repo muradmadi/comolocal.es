01b4ed99b4d5a1ec8d73a8dbb2cf9968c18c23ed
Murad Madi <muradmadi@gmail.com>
2026-01-24 15:41:51 +0100

    Fixed another minor error with redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://comolocal.es',
+  site: 'https://www.comolocal.es',
   output: 'server',
   trailingSlash: 'always',
   adapter: vercel(),
